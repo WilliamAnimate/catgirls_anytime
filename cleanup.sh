ea8cc804e12d3edfb3b52a846f9f6eb0780831c0
xandr. <xxdr@duck.com>
2024-07-25 01:26:37 -0500

    ci: ...there's now ci, docs: added links, etc etc i dunno man leav
    :horror:

--- cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# this code's purpose is to move all the .png files and .txt files to the ./old folder
-# in other words, this code just gets rid of the catgirl files lmfao
-
-mv *.png ./old
